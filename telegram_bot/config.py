0be2ea4564d58594c27c5df8c582c58b68b7571f
yaroslav_yudi <yaskih@mail.ru>
2021-06-08 20:40:00 +0300

    prod

--- telegram_bot/config.py
+++ telegram_bot/config.py
@@ -6,5 +6,5 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "1381540699:AAHw1Y8vTocoBHhlwcDhBXe
 PUBLIC_ADDRESS = os.getenv("PUBLIC_ADDRESS", "https://finance-adviser.leapper.com") #https://finance-adviser-bot.herokuapp.com
 RUNNING_ADDRESS = os.getenv("RUNNING_ADDRESS", "0.0.0.0")
 PORT = int(os.environ.get('PORT', '8443')) #8443
-API_URL = os.getenv("API_URL", "http://api:8000") #http://localhost:8000/api
+API_URL = os.getenv("API_URL", "http://api:8000/api") #http://localhost:8000/api
 # mongo_DB = 'LifeBot'
