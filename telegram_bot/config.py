3cb40e891393422a982ada1004d39daa1105eb1e
yaroslav_yudi <yaskih@mail.ru>
2021-06-08 19:22:05 +0300

    prod

--- telegram_bot/config.py
+++ telegram_bot/config.py
@@ -3,8 +3,8 @@ import os
 ENV = os.environ.get("ENV", "DEV")
 MONGO_URI = os.getenv("MONGO_URI",'mongodb+srv://root:root@cluster0.ro0oy.mongodb.net/test')
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "1381540699:AAHw1Y8vTocoBHhlwcDhBXeFU8KI_kSquEc")
-PUBLIC_ADDRESS = os.getenv("PUBLIC_ADDRESS", "https://finance-adviser-bot.herokuapp.com")
+PUBLIC_ADDRESS = os.getenv("PUBLIC_ADDRESS", "https://finance-adviser.leapper.com") #https://finance-adviser-bot.herokuapp.com
 RUNNING_ADDRESS = os.getenv("RUNNING_ADDRESS", "0.0.0.0")
-PORT = int(os.environ.get('PORT', '8443'))
-API_URL = os.getenv("API_URL", "http://localhost:8000/api")
+PORT = int(os.environ.get('PORT', '80')) #8443
+API_URL = os.getenv("API_URL", "https://finance-adviser.leapper.com/api") #http://localhost:8000/api
 # mongo_DB = 'LifeBot'
